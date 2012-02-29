c0bdb3356e430de1fda3627c9f01903ae64aed4c
yon <Lighthawky@gmail.com>
2012-02-29 18:08:47 +0200

    adding animation

---
+++ hobo_animation.js
@@ -0,0 +1,9 @@
+function HoboAnimation() {
+	this.file = "/resources/images/hobo.png";
+	this.height = 16;
+	this.width = 16;
+}
+
+HoboAnimation.prototype.getFrame = function(direction, animation){
+	
+};
