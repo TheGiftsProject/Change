54b4c157296882c05a62e4e7a59473bffdda033d
Asaf Gartner <agartner@ebay.com>
2012-02-29 18:01:11 +0200

    Added directional movement

--- hobo.js
+++ hobo.js
@@ -1,6 +1,7 @@
 function Hobo(x, y) {
     this.x = x;
     this.y = y;
+    this.direction = '';
 }
 
 Hobo.SIZE = {
@@ -12,18 +13,37 @@ Hobo.SPEED = 0.5;
 
 Hobo.prototype.update = function(keys) {
     if (keys.left) {
-        this.x -= Hobo.SPEED;
+        this.direction = 'left';
     }
     if (keys.up) {
-        this.y -= Hobo.SPEED;
+        this.direction = 'up';
     }
     if (keys.right) {
-        this.x += Hobo.SPEED;
+        this.direction = 'right';
     }
     if (keys.down) {
-        this.y += Hobo.SPEED;
+        this.direction = 'down';
     }
+
+    this.move();
 };
+
+Hobo.prototype.move = function() {
+    switch (this.direction) {
+        case 'left':
+            this.x -= Hobo.SPEED;
+            break;
+        case 'up':
+            this.y -= Hobo.SPEED;
+            break;
+        case 'right':
+            this.x += Hobo.SPEED;
+            break;
+        case 'down':
+            this.y += Hobo.SPEED;
+            break;
+    }
+}
 
 Hobo.prototype.render = function(ctx) {
     ctx.fillStyle = "#808080";
