48b2f4f33f8780f40ae528b78f55445b1899953e
yon <Lighthawky@gmail.com>
2012-03-14 18:28:51 +0200

    hobo resets road nicer

--- hobo.js
+++ hobo.js
@@ -6,7 +6,7 @@ function Hobo(x, y, world) {
     this.images = new HoboAnimation();
     this.points = 0;
     this.world = world;
-    this.world.getCellAt(Math.floor(this.x / Hobo.SIZE.w) ,Math.floor(this.y / Hobo.SIZE.h)).setAsPath();
+    this.world.getCellAt(this.currentRow(), this.currentCol()).setAsPath();
     this.world.collectAt(this.currentRow(), this.currentCol());
     SoundJS.add("die", "resources/sound/bitten.wav");
     SoundJS.add("coin", "resources/sound/coin.wav",5);
