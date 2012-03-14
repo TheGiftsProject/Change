3184b91abe1561369b692c07cb0aa93e91b10f7a
Adam Farhi <afarhi@ebay.com>
2012-03-14 09:35:31 -0700

    playing with maze variance

--- utils.js
+++ utils.js
@@ -1,10 +1,24 @@
-Math.roll = function(chance) {
-	return Math.random() < chance;
-}
+
+Math.roll = function(chance, variance_chance) {
+    variance_chance = variance_chance || 2;
+
+    var variance = 0;
+
+    var random_mod = 2;
+
+    var variance_chance_factor = parseInt(Math.random() * 100) % variance_chance == 0;
+    var random_factor = parseInt(3+(0-3)*Math.random()) % random_mod == 0;
+
+    if( variance_chance_factor  && random_factor === true  ) {
+        variance = Math.random();
+    }
+
+	return Math.random() < (chance + variance);
+};
 
 Number.prototype.mod = function(n) {
     return ((this % n) + n) % n;
-}
+};
 
 Array.prototype.shuffle = function() {
 	for(var j, x, i = this.length; i; j = parseInt(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
