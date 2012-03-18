a1492b086a58e40a437c935f1c5825567f9876ec
Adam Farhi <afarhi@ebay.com>
2012-03-18 10:03:41 -0700

    got Array.prototype.shuffle BACK

--- utils.js
+++ utils.js
@@ -20,3 +20,8 @@ Math.roll = function(chance) {
 Math.randomRange = function(A,B) {
     return B + (A-B) * Math.random();
 };
+
+Array.prototype.shuffle = function() {
+    for(var j, x, i = this.length; i; j = parseInt(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
+    return this;
+};
