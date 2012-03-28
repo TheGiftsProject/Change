6036592b5801c765edfe5a5af6c6993659a19822
yon <Lighthawky@gmail.com>
2012-03-28 17:57:42 +0200

    better menus

--- public/javascripts/menus.js
+++ public/javascripts/menus.js
@@ -1,28 +1,36 @@
 function Menus(){
     this.screens = {
         start: $(".screen.start"),
-//        pause: $(".screen.pause"),
-        game: $(".screen.game")
-//        highscore: $(".screen.highscore"),
-//        gameover: $(".screen.gameover"),
-//        about: $(".screen.about")
+        pause: $(".screen.pause"),
+        game: $(".screen.game"),
+        highscore: $(".screen.highscore"),
+        gameover: $(".screen.gameover"),
+        about: $(".screen.about")
     };
 
     this.screens.start.show();
     var that = this;
-    this.screens.start.find(".startgame").click(function(){
-        console.log("HUH?");
-        that.slideUp(that.screens.start);
+    $(".to_start").click(function(){
+        that.goto(that.screens.start);
+    });
+    $(".to_game").click(function(){
         that.game();
-    })
+    });
+    $(".to_about").click(function(){
+        that.goto(that.screens.about);
+    });
 }
 
-Menus.prototype.slideUp = function(screen){
-    screen.css({top:"-744px"});
+Menus.prototype.goto = function(screen){
+    var currentScreen = $(".screen:visible");
+    currentScreen.css({top:"-744px"});
+
+    screen.css({top:"0"});
+    screen.show()
 };
 
 Menus.prototype.game = function(){
-    this.screens.game.show();
+    this.goto(this.screens.game);
     window.hoboman = new HoboMan(document.getElementsByTagName('canvas')[0]);
 };
 
